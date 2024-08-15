281463fdd649d70d9aa706ae2a8865285aef5534
dz1nn <carlos.dlimacontato@gmail.com>
2024-08-14 21:14:37 -0300

    Finalizado o Desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner 
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Conta!");
+        numeroConta = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = entrada.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+
+        nomeCliente = entrada.nextLine();
+
+
+        System.out.println("Por favor, digite o saldo da Conta!");
+        saldo = Double.parseDouble(entrada.nextLine());
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo: R$ " + saldo +".Já está disponível para saque!");
+    
+        entrada.close();
     }
 }
