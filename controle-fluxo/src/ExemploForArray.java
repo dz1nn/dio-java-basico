1a10b6577e332e65713b5dbf1bbc9f1be4f07bc7
dz1nn <carlos.dlimacontato@gmail.com>
2024-08-18 11:22:46 -0300

    Continuação dos estudos

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+
+public class ExemploForArray {
+        public static void main(String[] args) {
+            
+        
+        String alunos [] = {"Daniel", "carlos", "lucas", "danielo"};
+
+        for (int x = 0; x < alunos.length; x++){
+            System.out.println("o aluno indece x= " + x + " é: " + alunos[x]);
+        }
+    }
+}
